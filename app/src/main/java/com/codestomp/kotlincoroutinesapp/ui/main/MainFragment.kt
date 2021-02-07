72de4e5c082360c2134b9cd306e065500c5a154c
Anwar Samir <anwarmadkour95@gmail.com>
2021-02-07 10:15:41 +0200

    - Scope

--- app/src/main/java/com/codestomp/kotlincoroutinesapp/ui/main/MainFragment.kt
+++ app/src/main/java/com/codestomp/kotlincoroutinesapp/ui/main/MainFragment.kt
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
 import com.codestomp.kotlincoroutinesapp.R
 import com.codestomp.kotlincoroutinesapp.data.models.Data
 import com.codestomp.kotlincoroutinesapp.data.models.RecipesResponse
@@ -17,73 +19,103 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class MainFragment : Fragment() {
 
+class MainFragment : Fragment() {
     private lateinit var binding: MainFragmentBinding
 
     companion object {
         fun newInstance() = MainFragment()
     }
 
+    val myScope = CoroutineScope(Dispatchers.Main + CoroutineName("AnwarScope"))
 
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
-         binding = MainFragmentBinding.inflate(inflater, container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = MainFragmentBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        // get all recipes from server
-        loadRecipesData()
+        myScope.launch {
+            binding.progressCircular.visibility=View.VISIBLE
+            val response=makeApiCall()
+            if (response.isSuccessful)
+            {
+                binding.progressCircular.visibility=View.GONE
+                buildRecipeRV(response.body()!!.data)
+            }else{
+                binding.progressCircular.visibility=View.GONE
+            }
+
+        }
+
+        binding.message.setOnClickListener {
+            Toast.makeText(requireActivity(),"this",Toast.LENGTH_LONG).show()
+        }
+
+    }
+
+    suspend fun makeApiCall(): Response<RecipesResponse> {
+
+        return withContext(Dispatchers.IO) {
+            ApiInterface().getRecipes()
+        }
 
     }
 
 
-    private fun loadRecipesData() {
 
 
-        binding.progressCircular.visibility=View.VISIBLE
-        ApiInterface().getRecipes().enqueue(object : Callback<RecipesResponse> {
+
+    private fun loadRecipesDataCall() {
+
+
+        binding.progressCircular.visibility = View.VISIBLE
+        ApiInterface().getRecipesCall().enqueue(object : Callback<RecipesResponse> {
             override fun onResponse(
                 call: Call<RecipesResponse>,
                 response: Response<RecipesResponse>
             ) {
-                binding.progressCircular.visibility=View.GONE
+                binding.progressCircular.visibility = View.GONE
                 buildRecipeRV(response.body()!!.data)
             }
 
             override fun onFailure(call: Call<RecipesResponse>, t: Throwable) {
-                binding.progressCircular.visibility=View.GONE
+                binding.progressCircular.visibility = View.GONE
             }
 
         })
     }
 
     private fun buildRecipeRV(data: ArrayList<Data>) {
-        val recipesAdapter=RecipesAdapter(data,object :RecipesAdapter.OnItemCLicked{
+        val recipesAdapter = RecipesAdapter(data, object : RecipesAdapter.OnItemCLicked {
             override fun onClick(pos: Int) {
                 TODO("Not yet implemented")
             }
         })
 
-        binding.rvRecipes.adapter=recipesAdapter
+        binding.rvRecipes.adapter = recipesAdapter
     }
 
 
-     fun doHeavyTask()
-    {
-        for ( i in 1..100000000000000)
-        {
-            val x= i*22323
-            val r= i*22323
-            val c= i*22323
-            val z= i*22323
-            val t= i*22323
+    fun doHeavyTask() {
+        for (i in 1..100000000000000) {
+            val x = i * 22323
+            val r = i * 22323
+            val c = i * 22323
+            val z = i * 22323
+            val t = i * 22323
 
 
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
     }
 }
